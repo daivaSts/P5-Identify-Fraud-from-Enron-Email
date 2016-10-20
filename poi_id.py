1714abca6879defd6836748906a3a12affbb23ef
daivaSts <daiva.satas@gmail.com>
2016-10-19 19:45:21 -0500

    revision #2

--- poi_id.py
+++ poi_id.py
@@ -22,6 +22,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.decomposition import PCA, RandomizedPCA
 from sklearn.feature_selection import SelectKBest
 
+from helper_functions import *
+
 
 ##############################################################################
 ### Task 1: Selecting the features to use.
@@ -41,119 +43,9 @@ features_list = ["poi","salary", "deferral_payments", "total_payments",\
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-##############################################################################
-### Task 2: Helper functions
-##############################################################################
-
-# Helper function to create new fraction_from_poi & fraction_to_poi features.
-# Credit: www.udacity.com/course/intro-to-machine-learning--ud120
-# course developers.
-def computeFraction( poi_messages, all_messages ):
-    """ given a number messages to/from POI (numerator) and number of all
-        messages to/from a person (denominator), return the fraction of
-        messages to/from that person that are from/to a POI.
-   	"""
-    fraction = 0.
-    if poi_messages == "NaN" or all_messages == "NaN":
-        fraction = 0.
-    else:
-        fraction = float(poi_messages) / all_messages
-    return fraction
-
-
-# Helper function to separate features to target list and remaining features
-# list. Credit: www.udacity.com/course/intro-to-machine-learning--ud120
-# course developers.
-def targetFeatureSplit( data ):
-    """ 
-        given a numpy array, separate out the first feature
-        and put it into its own list (this should be the 
-        quantity you want to predict)
-
-        return targets and features as separate lists
-    """
-    target = []
-    features = []
-    for item in data:
-        target.append( item[0] )
-        features.append( item[1:] )
-
-    return target, features
- 
-
-# Helper functions to convert data dictionery to numpy array of features
-# Credit: www.udacity.com/course/intro-to-machine-learning--ud120 course
-# developers
-def featureFormat( dictionary,features,remove_NaN=True,remove_all_zeroes=True,\
-				  remove_any_zeroes=False, sort_keys = False):
-    """ convert dictionary to numpy array of features
-        remove_NaN = True will convert "NaN" string to 0.0
-        remove_all_zeroes = True will omit any data points for which
-            all the features you seek are 0.0
-        remove_any_zeroes = True will omit any data points for which
-            any of the features you seek are 0.0
-        sort_keys = True sorts keys by alphabetical order. Setting the value as
-            a string opens the corresponding pickle file with a preset key
-            order (this is used for Python 3 compatibility, and sort_keys
-            should be left as False for the course mini-projects).
-        NOTE: first feature is assumed to be 'poi' and is not checked for
-            removal for zero or missing values.
-    """
-    return_list = []
-
-    # Key order - first branch is for Python 3 compatibility on mini-projects,
-    # second branch is for compatibility on final project.
-    if isinstance(sort_keys, str):
-        import pickle
-        keys = pickle.load(open(sort_keys, "rb"))
-    elif sort_keys:
-        keys = sorted(dictionary.keys())
-    else:
-        keys = dictionary.keys()
-
-    for key in keys:
-        tmp_list = []
-        for feature in features:
-            try:
-                dictionary[key][feature]
-            except KeyError:
-                print "error: key ", feature, " not present"
-                return
-            value = dictionary[key][feature]
-            if value=="NaN" and remove_NaN:
-                value = 0
-            tmp_list.append( float(value) )
-
-        # Logic for deciding whether or not to add the data point.
-        append = True
-        # exclude 'poi' class as criteria.
-        if features[0] == "poi":
-            test_list = tmp_list[1:]
-        else:
-            test_list = tmp_list
-        ### if all features are zero and you want to remove
-        ### data points that are all zero, do that here
-        if remove_all_zeroes:
-            append = False
-            for item in test_list:
-                if item != 0 and item != "NaN":
-                    append = True
-                    break
-        ### if any features for a given data point are zero
-        ### and you want to remove data points with any zeroes,
-        ### handle that here
-        if remove_any_zeroes:
-            if 0 in test_list or "NaN" in test_list:
-                append = False
-        ### Append the data point if flagged for addition.
-
-        if append:
-            return_list.append( np.array(tmp_list) )
-
-    return np.array(return_list)
 
 ##############################################################################
-## Task 3: Remove outliers
+## Task 2: Remove outliers
 ##############################################################################
 
 # Analyzing the data set:
@@ -207,7 +99,7 @@ if False:
 
 
 ##############################################################################
-### Task 4: Create new feature(s)
+### Task 3: Create new feature(s)
 ##############################################################################
 
 # Adding new features for each data point to data_dict. Credit: 
@@ -226,7 +118,7 @@ for name in data_dict:
     data_point["fraction_to_poi"] = fraction_to_poi
 
 # Adding new features to features list for training and/or testing
-# and removed the features used to calculate these new ones.
+# and removing the features used to calculate these new ones.
 if True:
 	features_list.append("fraction_from_poi")
 	features_list.append("fraction_to_poi")
@@ -235,7 +127,6 @@ if True:
 	features_list.remove( "from_this_person_to_poi")
 	features_list.remove( "to_messages" )
 	features_list.remove( "from_messages" )
-
 
 
 # Checking if exercised_stock_options, restricted_stock could be removed,
@@ -292,7 +183,7 @@ if False:
 
 
 ##############################################################################
-### Task 5: Try a variety of classifiers
+### Task 4: Try a variety of classifiers
 ##############################################################################
 
 features_train, features_test, labels_train, labels_test = \
@@ -398,25 +289,21 @@ if False:
  
 
 ###############################################################################
-### Task 6: Tune the classifier to achieve better than .3 precision and recall 
+### Task 5: Tune the classifier to achieve better than .3 precision and recall 
 ### using our testing script. 
 ##############################################################################
-
-features_train, features_test, labels_train, labels_test = \
-		cross_validation.train_test_split(features, labels,\
-										 test_size=0.35, random_state=42)
 
 
 # Decision Tree Classifier
 if True:
-
-
 	#Split features and labels to test/ train subsets
 	acc_ave = 0
 	recall_sc_ave = 0
 	precision_sc_ave = 0
-	f1_sc_ave =0
-	folds = 4
+	f1_sc_ave = 0
+	expl_var_ave = 0
+	folds = 6
+	n_components = 10
 
 
 	kf = cross_validation.KFold(len(labels), folds)
@@ -428,39 +315,32 @@ if True:
 		features_test = [features[i] for i in test_indices]
 		labels_train = [labels[i] for i in train_indices]
 		labels_test = [labels[i] for i in test_indices]
-		
-		# Transforms features by scaling each feature to [0, 1] range
-		min_max_scaler = preprocessing.MinMaxScaler()
-		features_train_scaled = min_max_scaler.fit_transform(features_train)
-		features_test_scaled = min_max_scaler.fit_transform(features_test)
-
 
 		# Reduce the feature dimentions using Principal component analysis PCA
-		n_components = 5
 		pca = PCA( n_components = n_components, whiten = True)
-		pca.fit( features_train_scaled)
+		pca.fit( features_train)
 
-
-		# TEST: plot first pca component to test and visualize
+		# TEST: pca components
 		if False:
 			pca_first = pca.components_[0]
 			pca_second = pca.components_[1]
-			print "pca first component: {}".format( pca_first)
-			print "***"	
+			#print "pca first component: {}".format( pca_first)
+
+			#print "***"	
 
 		# Select features according to the k highest scores
-		selection = SelectKBest( k=5)
+		selection = SelectKBest(k=9)
 
 		# Build estimator from PCA and Univariate SelectKBest selection:
 		combined_features = FeatureUnion([("pca", pca),\
 										 ("univ_select", selection)])
 
 		# Use combined train features to fit and transform train subset:
-		features_train_reduced = combined_features.fit( features_train_scaled,\
-								 labels_train ).transform( features_train_scaled )
+		features_train_reduced = combined_features.fit( features_train,\
+								 labels_train ).transform( features_train )
 
 		# Use combined test features to transform test subset:
-		features_test_reduced = combined_features.transform( features_test_scaled )
+		features_test_reduced = combined_features.transform( features_test )
 
 		# parameters to be passed to GridSearchCV for cross validation
 		parameters  = {"criterion" : ["gini", "entropy"],
@@ -489,12 +369,13 @@ if True:
 		precision_sc = precision_score( labels_test, pred )
 		f1_sc = f1_score( labels_test, pred )
 
+
 		# calculatting total of the metrics
 		acc_ave += acc
 		recall_sc_ave += recall_sc
 		precision_sc_ave += precision_sc
 		f1_sc_ave += f1_sc
-
+		
 
 	# printing averages of the metrics
 	if True:
@@ -502,8 +383,7 @@ if True:
 		print "DT average recall_score: {}".format( round( recall_sc_ave / folds,3) )
 		print "DT average precision_score: {}".format( round( precision_sc_ave / folds, 3))
 		print "DT average f1_score: {}\n".format( round( f1_sc_ave/ folds , 3))	
-		print "DT best parameters : {}".format(clf.best_params_)
-		
+
 
 # GaussianNB Classifier
 if False:
